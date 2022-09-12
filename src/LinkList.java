99a30d2ace2528afc4bc77d980b04db02a3ee1c5
Xagar123 <sagarcoc2000@gmail.com>
2022-09-12 18:36:02 +0530

    Adding data in linked list

--- src/LinkList.java
+++ src/LinkList.java
@@ -1,37 +1,58 @@
 
-    class Node {
-        private int data;
-        private Node next;
+class Node {
+    public int data;
+    public Node next;
 
-        //Node Constructor for Creating New Node
-        Node(int data) {
-            this.data = data;
-            this.next = null;
+    //Node Constructor for Creating New Node
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    Node head;
+    Node tail;
+    //Insert Data in New Node
+    public void insertFirst(int data) {
+        Node newNode = new Node(data);
+        //Adding Data in Node
+        if(head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
         }
     }
 
-    class LinkedList {
-        Node head;
-        Node tail;
-        //Insert Data in New Node
-        public void insertFirst(int data) {
-            Node newNode = new Node(data);
+    //Display Node in Linked List
+    public void showLinkedList() {
+        if(head == null) {
+            System.out.println("Linked List is Empty.");
+        } else {
+            Node temp = head;
+            while (temp != null) {
+                System.out.print(temp.data + " -> ");
+                temp = temp.next;
+            }
         }
     }
-    public class LinkList {
+}
 
-        public static void main(String[] args) {
-            //Welcome Message
-            System.out.println("Welcome to LinkedList Data Structures Problems.");
+public class LinkList {
+    public static void main(String[] args) {
+        //Welcome Message
+        System.out.println("Welcome to LinkedList Data Structures Problem.");
 
-            LinkedList linkedList = new LinkedList();
-            //Insert 50, 30, 70 in Linked List Nodes
+        LinkedList linkedList = new LinkedList();
 
-            linkedList.insertFirst(56);
-            linkedList.insertFirst(30);
-            linkedList.insertFirst(70);
+        //Insert 70, 50, 36 in Linked List Nodes
+        linkedList.insertFirst(70);
+        linkedList.insertFirst(30);
+        linkedList.insertFirst(56);
 
-
-        }
+        //Showing Linked List Data
+        linkedList.showLinkedList();
     }
-
+}
